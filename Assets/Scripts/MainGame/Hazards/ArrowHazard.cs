0e857ab05faabf2fca6187c5781167b1b6f4c07b
MatanBelfer <belfer.matan@gmail.com>
2026-02-21 11:24:23 +0200

    Add pooling and lifetime to arrow hazards
    Replace per-frame Instantiate logic with an object pool for arrows to improve performance. ArrowHazard now uses a prewarmed Queue, a ShootLoop coroutine, arrowRotationOffsetEuler and initialPoolSize fields, and exposes Get/Return/CreateNew methods. ArrowObject gains maxLifetime, Init(owner), and ReturnToPool with a guard to recycle arrows back to the pool when expired. Prefabs and scene instances updated to include collider/rigidbody settings, rotation offsets, initial pool size and lifetime on arrow prefab.

--- Assets/Scripts/MainGame/Hazards/ArrowHazard.cs
+++ Assets/Scripts/MainGame/Hazards/ArrowHazard.cs
@@ -1,38 +1,79 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ArrowHazard : MonoBehaviour
 {
-    public GameObject arrowPrefab;
-    [SerializeField] float shootInterval;
-    private float shootIntervalLeft;
-    
+    [Header("Arrow")]
+    [SerializeField] private ArrowObject arrowPrefab;
+    [SerializeField] private Vector3 arrowRotationOffsetEuler = new Vector3(0f, 180f, 0f);
+
+    [Header("Shooting")]
+    [SerializeField] private float shootInterval = 1f;
+
+    [Header("Pool")]
+    [SerializeField] private int initialPoolSize = 8;
+
+    private readonly Queue<ArrowObject> _pool = new Queue<ArrowObject>();
+    private Coroutine _shootRoutine;
+
     private void Awake()
     {
-        //TODO remove
+        Prewarm();
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnEnable()
     {
-        shootIntervalLeft = shootInterval;
+        if (_shootRoutine == null)
+            _shootRoutine = StartCoroutine(ShootLoop());
     }
 
-    
-    //TODO change from update to timed based call for shooting with pooling 
-    
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        shootIntervalLeft -= Time.deltaTime;
-        if (shootIntervalLeft <= 0)
+        if (_shootRoutine != null)
         {
-            
-            //TODO change to pool
-            ArrowObject arrow = Instantiate(arrowPrefab,transform.position,Quaternion.identity).GetComponent<ArrowObject>();
-            arrow.transform.Rotate(0,90,0);
-            arrow.transform.Rotate(0,90,0);
-            shootIntervalLeft = shootInterval;
+            StopCoroutine(_shootRoutine);
+            _shootRoutine = null;
         }
+    }
+
+    private void Prewarm()
+    {
+        for (int i = 0; i < initialPoolSize; i++)
+            Return(CreateNew());
+    }
+
+    private IEnumerator ShootLoop()
+    {
+        var wait = new WaitForSeconds(shootInterval);
+        while (true)
+        {
+            Get(transform.position, transform.rotation * Quaternion.Euler(arrowRotationOffsetEuler));
+            yield return wait;
+        }
+    }
+
+    private ArrowObject Get(Vector3 position, Quaternion rotation)
+    {
+        ArrowObject arrow = _pool.Count > 0 ? _pool.Dequeue() : CreateNew();
+        arrow.transform.SetPositionAndRotation(position, rotation);
+        arrow.gameObject.SetActive(true);
+        return arrow;
+    }
+
+    public void Return(ArrowObject arrow)
+    {
+        arrow.gameObject.SetActive(false);
+        arrow.transform.SetParent(null);
+        _pool.Enqueue(arrow);
+    }
+
+    private ArrowObject CreateNew()
+    {
+        ArrowObject arrow = Instantiate(arrowPrefab);
+        arrow.gameObject.SetActive(false);
+        arrow.Init(this);
+        return arrow;
     }
 }
--- Assets/Scripts/MainGame/Hazards/ArrowObject.cs
+++ Assets/Scripts/MainGame/Hazards/ArrowObject.cs
@@ -1,19 +1,40 @@
+using System;
 using UnityEngine;
 
-public class ArrowObject : MonoBehaviour 
+public class ArrowObject : MonoBehaviour
 {
-    public float speed;
-    public float damage;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] public float speed = 10f;
+    [SerializeField] public float damage = 1f;
+    [SerializeField] private float maxLifetime = 5f;
+
+    private ArrowHazard _owner;
+    private float _lifeLeft;
+    private bool _isReturned;
+
+    public void Init(ArrowHazard owner)
     {
-        //TODO remove
+        _owner = owner;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
+    {
+        _lifeLeft = maxLifetime;
+        _isReturned = false;
+    }
+
+    private void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        _lifeLeft -= Time.deltaTime;
+        if (_lifeLeft <= 0f)
+            ReturnToPool();
+    }
+
+    private void ReturnToPool()
+    {
+        if (_isReturned) return;
+        _isReturned = true;
+        _owner.Return(this);
     }
 }
